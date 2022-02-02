62f085c973aa914ea00a114c8b05d26d7b847955
remer mendez <carvajalremer@gmail.com>
2022-02-02 11:00:04 -0400

    entrenamiento1

--- ejercicios/ejercicio4.js
+++ ejercicios/ejercicio4.js
@@ -5,11 +5,18 @@ prompt.start();
 prompt.get(['numero'], function (err , result){
     let numero = result.numero;
 
-    if (numero < 0){
-        console.log("el numero " + numero + " es negativo");
+    if (numero < 0 && numero % 2 == 0){
+        console.log("el numero " + numero + " par es negativo");
+    }
+    else if (numero < 0 && numero % 2 != 0){
+        console.log("el numero " + numero + " impar es negativo");
+    }
+
+    else if ( numero > 0 && numero % 2 == 0){
+        console.log("el numero " + numero + " par es positivo")
     }
     else{
-        console.log("el numero es " + numero + " es positivo")
+        console.log("el numero " + numero + " impar es positivo");
     }
 
 });
