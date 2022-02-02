6b90cf391d9d0832efb535516690a26dc7fe4299
remer mendez <carvajalremer@gmail.com>
2022-02-02 09:39:12 -0400

    entrenamiento1

--- ejercicios/ejercicio6.js
+++ ejercicios/ejercicio6.js
@@ -2,10 +2,10 @@ const prompt = require('prompt');
 
 prompt.start();
 
-prompt.get(['diametro'], function (err , result){
+prompt.get(['diametro','grosor'], function (err , result){
     let diametro = result.diametro;
 
-    if (diametro > 1.4 ){
+    if (diametro > 1.4){
         console.log("la rueda es para un vehiculo grande.");
     }
     else if (diametro <= 1.4 && diametro > 0.8){
@@ -13,5 +13,14 @@ prompt.get(['diametro'], function (err , result){
     }
     else{
         console.log("la rueda es para un vehiculo pequeño");
+    }
+
+    let grosor = result.grosor;
+
+    if (diametro > 1.4 && grosor < 0.4){
+        console.log("el grosor de esta rueda es inferior a lo recomendado");
+    }
+    else if (diametro <= 1.4 && diametro > 0.8 && grosor < 0.25){
+        console.log("el grosor de esta rueda es inferior a lo recomendado");
     }
 });
